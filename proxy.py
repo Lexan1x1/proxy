b0b0dadaeceb89859cd6153a49893341715891d7
Lexan1x1 <118561784+Lexan1x1@users.noreply.github.com>
2024-01-29 15:52:00 +0300

    Update and rename lex.py to proxy.py

--- proxy.py
+++ proxy.py
@@ -1,9 +1,15 @@
+# -*- coding: utf-8 -*-
+# author Dinar Ahmetshin 
+# site https://ahmetshin.com
 import sys 
 import os
 
 def main():
     username="userproxy"
-    password_proxy="root"
+    try:
+        password_proxy =raw_input("type your password here: ")
+    except:
+        password_proxy =input("type your password here: ")
     os.system("apt-get update")
     os.system("apt-get -y install build-essential libwrap0-dev libpam0g-dev libkrb5-dev libsasl2-dev")
     os.system("wget --no-check-certificate https://ahmetshin.com/static/dante.tgz")
@@ -35,12 +41,15 @@ def main():
             }' > /home/dante/danted.conf
                 """)
     os.system("useradd --shell /usr/sbin/nologin -m %s" % username)
+    os.system('echo "%s:%s" | chpasswd' % (username,password_proxy))
     os.system("apt-get -y install ufw")
     os.system("ufw status")
     os.system("ufw allow ssh")
     os.system("ufw allow proto tcp from any to any port 1080")
     os.system("ufw status numbered")
+    os.system("echo 'y' | ufw enable")
     os.system("""
+echo '#!/bin/sh -e
 sleep 20
 /home/dante/sbin/sockd -f /home/dante/danted.conf -D
 exit 0
@@ -49,6 +58,14 @@ exit 0
     os.system("chmod +x /etc/rc.local")
     os.system("chmod +x /home/dante/sbin/sockd")
     os.system("/home/dante/sbin/sockd -f /home/dante/danted.conf -D")
+    os.system("echo 'proxy install success'")
+    os.system("echo ' '")
+    os.system("echo '________________________________'")
+    os.system("echo ' '")
+    os.system("echo \"YOUR IP ADDRESS: `hostname -I | awk '{print $1}'`\"")
+    os.system("echo 'PORT: 1080'")
+    os.system("echo 'LOGIN: %s'" % username)
+    os.system("echo 'PASSWORD: %s'" % password_proxy)
 
 if __name__ == "__main__":
     main()
